4e5012740cbfbc97022613402a4a683836647bc2
Anillab <anillabusolo@gmail.com>
2020-03-05 14:05:00 +0000

    added props

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -2,37 +2,17 @@ import React from 'react';
 import {Table} from 'react-bootstrap';
 
 
-const Group=() =>{
+
+const Group=(props) =>{
   return(
-<Table striped bordered hover>
-  <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>{props.obj.name}</th>
+      <th>{props.obj.description}</th>
+      <th>{props.obj.bankAccount}</th>
+      <th>{props.obj.frequency}</th>
+
+
     </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</Table>
 )
 }
 export default Group;
