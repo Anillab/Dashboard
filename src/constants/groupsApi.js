d799f89502ededc1bf69c12c8c0365f269f1be63
Anillab <anillabusolo@gmail.com>
2020-03-05 16:32:15 +0000

    installed packages

---
+++ src/constants/groupsApi.js
@@ -0,0 +1,49 @@
+import React ,{Component} from 'react';
+import axios from 'axios';
+import Group from '../components/Groups.jsx';
+import {Table} from 'react-bootstrap';
+
+
+export default class InfoGroup extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { groupCollection: [] };
+    }
+
+    componentDidMount(){
+      axios.get('https://1e9100c6.ngrok.io/api/groups')
+            .then(res =>{
+              console.log(res.data);
+              this.setState({groupCollection:res.data.groups})
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
+    }
+
+    groupTable(){
+      console.log(this.state.groupCollection);
+      return this.state.groupCollection.map((data,i)=>{
+        return <Group obj={data} key={i} />;
+      });
+    }
+    render(){
+      return(
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th> Name</th>
+              <th>Description</th>
+              <th>Account</th>
+              <th>Frequency</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {this.groupTable()}
+          </tbody>
+        </Table>
+      )
+    }
+}
