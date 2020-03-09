b7d6a1406a3ed3900eb1549c98211be116be29d5
Anillab <anillabusolo@gmail.com>
2020-03-09 14:59:28 +0000

    added the length of members

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -8,7 +8,7 @@ const Group=(props) =>{
       <th>{props.obj.name}</th>
       <th>{props.obj.description}</th>
       <th>{props.obj.bankAccount}</th>
-      <th>{props.obj.frequency}</th>
+      <th>{props.obj.members.length}</th>
 
 
     </tr>
