84461fc013b16040d36ecdd345053151f8af07c5
Anillab <anillabusolo@gmail.com>
2020-03-09 15:01:01 +0000

    modified the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Nav,Button, Navbar,Brand,Toggle} from 'react-bootstrap'
+import {Nav,Button, Navbar,Brand,Toggle} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FiBell } from "react-icons/fi";
 
