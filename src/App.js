4a5ac039560530fb4ed1d53ca12b3860c924fac3
Anillab <anillabusolo@gmail.com>
2020-03-09 14:49:17 +0000

    imported css and bootstrap files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import {
   Switch
 } from 'react-router-dom';
 // import Dashboard from './components/Dashboard';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
 import MainRoute from './routes/routes.js'
 
 const MainHome = props => {
