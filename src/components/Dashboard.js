dc822819ce0b2012a0249e00838683b045864467
Anillab <anillabusolo@gmail.com>
2020-03-10 17:58:47 +0000

    removed the tasks card

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Card } from "./Card.jsx";
 import { StatsCard } from "./StatsCard.jsx";
 import { Tasks } from "./Task.jsx";
-import AccountData from './search.jsx'
+import AccountData from './search.jsx';
+import ChartsPage from './BarChart.jsx';
+import LinePage from './LineChart.jsx';
+import VerticalPage from './VerticalBar.jsx';
+
 import {
   dataPie,
   legendPie,
@@ -35,7 +39,11 @@ class Dashboard extends Component {
       <div className="content">
         <Container fluid>
         <Row>
+        <Col md={6}>
+
         <AccountData />
+        </Col>
+
         </Row>
           <Row>
             <Col lg={3} sm={6}>
@@ -50,10 +58,10 @@ class Dashboard extends Component {
             <Col lg={3} sm={6}>
               <StatsCard
                 bigIcon={<i className="pe-7s-wallet text-success" />}
-                statsText="Revenue"
-                statsValue="$1,345"
+                statsText="Total Contribution"
+                statsValue="GHC 12000"
                 statsIcon={<i className="fa fa-calendar-o" />}
-                statsIconText="Last day"
+                statsIconText="Updated 6 Hours Ago"
               />
             </Col>
             <Col lg={3} sm={6}>
@@ -68,10 +76,10 @@ class Dashboard extends Component {
             <Col lg={3} sm={6}>
               <StatsCard
                 bigIcon={<i className="fa fa-twitter text-info" />}
-                statsText="Followers"
-                statsValue="+45"
+                statsText="Loan Requeets"
+                statsValue="45"
                 statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
+                statsIconText="Updated 2 days Ago"
               />
             </Col>
           </Row>
@@ -80,47 +88,37 @@ class Dashboard extends Component {
               <Card
                 statsIcon="fa fa-history"
                 id="chartHours"
-                title="Users Behavior"
+                title="Groups Behavior"
                 category="24 Hours performance"
-                stats="Updated 3 minutes ago"
                 content={
                   <div className="ct-chart">
-                    <ChartistGraph
-                      data={dataSales}
-                      type="Line"
-                      options={optionsSales}
-                      responsiveOptions={responsiveSales}
-                    />
+                    <LinePage  />
                   </div>
                 }
-                legend={
-                  <div className="legend">{this.createLegend(legendSales)}</div>
-                }
+
               />
             </Col>
             <Col md={4}>
               <Card
                 statsIcon="fa fa-clock-o"
-                title="Email Statistics"
-                category="Last Campaign Performance"
-                stats="Campaign sent 2 days ago"
+                title="Group  Statistics"
+                category="Types of Groups"
+                stats="Updated 5 days Ago"
                 content={
                   <div
                     id="chartPreferences"
                     className="ct-chart ct-perfect-fourth"
                   >
-                    <ChartistGraph data={dataPie} type="Pie" />
+                    <ChartsPage />
                   </div>
                 }
-                legend={
-                  <div className="legend">{this.createLegend(legendPie)}</div>
-                }
+
               />
             </Col>
           </Row>
 
           <Row>
-            <Col md={6}>
+            <Col md={12}>
               <Card
                 id="chartActivity"
                 title="2014 Sales"
@@ -129,33 +127,10 @@ class Dashboard extends Component {
                 statsIcon="fa fa-check"
                 content={
                   <div className="ct-chart">
-                    <ChartistGraph
-                      data={dataBar}
-                      type="Bar"
-                      options={optionsBar}
-                      responsiveOptions={responsiveBar}
-                    />
+                    <VerticalPage  />
                   </div>
                 }
-                legend={
-                  <div className="legend">{this.createLegend(legendBar)}</div>
-                }
-              />
-            </Col>
 
-            <Col md={6}>
-              <Card
-                title="Tasks"
-                category="Backend development"
-                stats="Updated 3 minutes ago"
-                statsIcon="fa fa-history"
-                content={
-                  <div className="table-full-width">
-                    <table className="table">
-                      <Tasks />
-                    </table>
-                  </div>
-                }
               />
             </Col>
           </Row>
