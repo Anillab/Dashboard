871b8cd98e03bacb5325a48be222c75653dc9436
Anillab <anillabusolo@gmail.com>
2020-03-12 22:39:34 +0000

    removed unused files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import {Nav,Button, Navbar,Brand,Toggle} from 'react-bootstrap';
+import {Nav, Navbar} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FiBell } from "react-icons/fi";
-
+import Credbook from "../assets/Credbook.png";
+import Image from 'react-render-image';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = (props)=>{
   return(
     <>
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Credbook</Navbar.Brand>
+<Navbar bg="light" expand="lg" variant="crimson">
+  <Navbar.Brand>
+  <Image src={Credbook}>
+   {({image})=>{
+     return <img src={Credbook} width={'200px'} height={'75px'} alt={Credbook}/>;
+   }}
+  </Image>
+  </Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#home">Contact Us hello@credbook.com</Nav.Link>
       <Nav.Link href="#link">TEL:+2331345761</Nav.Link>
-      <Nav.Link href="#link">< FiBell/></Nav.Link>
-
+      <Link to='/notification'>
+      <Nav.Link>< FiBell/></Nav.Link>
+      </Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
