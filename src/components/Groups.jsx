894a604d666f052b19796642f90e2a218f8d5e49
Anillab <anillabusolo@gmail.com>
2020-03-12 22:35:37 +0000

    deleted unused modules

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
   Link,
-  Switch
 } from 'react-router-dom';
 
 
@@ -14,6 +11,7 @@ const Group=(props) =>{
       <th>{props.obj.name}</th>
       <th>{props.obj.description}</th>
       <th>{props.obj.bankAccount}</th>
+      <th>{props.obj.frequency}</th>
       <th>{props.obj.members.length}</th>
       <Link to='/groupStatement'>
       <th>{'statement'}</th>
