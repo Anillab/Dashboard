d95ba3aec9a68e9a4143abedb02d6b24f66d5e32
Anillab <anillabusolo@gmail.com>
2020-03-12 22:36:35 +0000

    changed variables name

--- src/components/VerticalBar.jsx
+++ src/components/VerticalBar.jsx
@@ -5,27 +5,23 @@ import { MDBContainer } from "mdbreact";
 class VerticalPage extends React.Component {
   state = {
     dataBar: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: ["Semi-Formal", "Informal", "Formal"],
       datasets: [
         {
-          label: "% of Votes",
-          data: [12, 19, 3, 5, 2, 3],
+          label: "Contributions",
+          data: [12, 19, 3],
           backgroundColor: [
             "rgba(255, 134,159,0.4)",
             "rgba(98,  182, 239,0.4)",
-            "rgba(255, 218, 128,0.4)",
-            "rgba(113, 205, 205,0.4)",
-            "rgba(170, 128, 252,0.4)",
-            "rgba(255, 177, 101,0.4)"
+            "rgba(255, 218, 128,0.4)"
+
           ],
           borderWidth: 2,
           borderColor: [
             "rgba(255, 134, 159, 1)",
             "rgba(98,  182, 239, 1)",
-            "rgba(255, 218, 128, 1)",
-            "rgba(113, 205, 205, 1)",
-            "rgba(170, 128, 252, 1)",
-            "rgba(255, 177, 101, 1)"
+            "rgba(255, 218, 128, 1)"
+
           ]
         }
       ]
