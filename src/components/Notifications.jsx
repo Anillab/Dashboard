ec28f1bb370c66c54bd2407c96ef23629d24ea4d
Anillab <anillabusolo@gmail.com>
2020-03-12 22:40:10 +0000

    added a file

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,45 +1,50 @@
-import React, { Component } from "react";
-import { MDBNotification, MDBContainer } from "mdbreact";
+import React from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
-class Notification extends Component {
+class Notification  extends React.Component {
+  createNotification = (type) => {
+    return () => {
+      switch (type) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'success':
+          NotificationManager.success('Success message', 'Title here');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+          break;
+        case 'error':
+          NotificationManager.error('Error message', 'Click me!', 5000, () => {
+            alert('callback');
+          });
+          break;
+      }
+    };
+  };
   render() {
-    return (
-      <MDBContainer>
-        <MDBNotification
-          show
-          fade
-          iconClassName="text-primary"
-          title="Bootstrap"
-          message="See? Just like this."
-          text="just now"
-        />
-        <MDBNotification
-          show
-          fade
-          iconClassName="text-primary"
-          title="Bootstrap"
-          message="Heads up, toasts will stack automatically"
-          text="2 seconds ago"
-        />
-        <MDBNotification
-          show
-          fade
-          iconClassName="text-primary"
-          title="Bootstrap"
-          message="Heads up, toasts will stack automatically"
-          text="2 seconds ago"
-        />
-        <MDBNotification
-          show
-          fade
-          iconClassName="text-primary"
-          title="Bootstrap"
-          message="Heads up, toasts will stack automatically"
-          text="2 seconds ago"
-        />
-      </MDBContainer>
-    );
-  }
-}
+     return (
+       <div>
+         <button className='btn btn-info'
+           onClick={this.createNotification('info')}>Info
+         </button>
+         <hr/>
+         <button className='btn btn-success'
+           onClick={this.createNotification('success')}>Success
+         </button>
+         <hr/>
+         <button className='btn btn-warning'
+           onClick={this.createNotification('warning')}>Warning
+         </button>
+         <hr/>
+         <button className='btn btn-danger'
+           onClick={this.createNotification('error')}>Error
+         </button>
 
-export default Notification;
+         <NotificationContainer/>
+       </div>
+     );
+   }
+ }
+
+ export default Notification;
