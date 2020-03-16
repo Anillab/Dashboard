c558769b3a0f0acab998cfcd8c162e8739c98984
Anillab <anillabusolo@gmail.com>
2020-03-16 09:45:45 +0000

    added onclick event on the row

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
 import {
-  Link
+  Link, useHistory
 } from 'react-router-dom';
 
 
 
 const Group=(props) =>{
+  let history = useHistory();
+  console.log(history)
   return(
-
-    <tr>
+    <tr onClick={() => history.push(`/dashboard/groups/${props.obj._id}`)}>
       <th>{props.obj.name}</th>
       <th>{props.obj.description}</th>
       <th>{props.obj.bankAccount}</th>
       <th>{props.obj.frequency}</th>
       <th>{props.obj.members.length}</th>
+
       <Link to='/groupStatement'>
       <th>{'statement'}</th>
       </Link>
 
-
     </tr>
+
 
 )
 }
