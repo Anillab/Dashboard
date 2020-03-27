4d448446be14ef0b2a1d5e5386921b3cf4140457
Anillab <anillabusolo@gmail.com>
2020-03-27 13:32:41 +0000

    fetched member info

--- src/constants/groupInfo.jsx
+++ src/constants/groupInfo.jsx
@@ -2,50 +2,85 @@ import React,{useEffect, useState} from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
 import Each from '../components/groupEach.jsx';
-import {Table} from 'react-bootstrap';
+import {Table, Card, Spinner} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const API_URL='https://hidden-atoll-66913.herokuapp.com/api';
 
 const DetailGroup = () => {
   const [groupDetails, setGroupDetails] = useState({})
-      const {id} = useParams()
-      console.log(id)
+  const [member, setMember] = useState([])
+
+  const {id} = useParams()
+  console.log(id)
+
   useEffect(() => {
     const fetchGroup = groupId => {
-      axios.get(`https://hidden-atoll-66913.herokuapp.com/api/groups/${groupId}`).then(res => {
-        console.log(res.data);
+      axios.get(`${API_URL}/groups/${groupId}`)
+      .then(res => {
         setGroupDetails({...groupDetails, ...res.data.group})
-      }).catch(error =>{
+      })
+      .catch(error =>{
         console.log(error);
       })
 
     };
 
     fetchGroup(id);
+
   }, [])
 
+  useEffect(() => {
+    const fetchMember = async memberId => {
+      try{
+         const res = await axios.get(`${API_URL}/members/${memberId}`)
+         return res.data.member
+      } catch(err){
+          console.log(err);
+      }
+    }
+
+    if(groupDetails.members){
+      Promise.all(groupDetails.members.map(memberId => fetchMember(memberId))).then(value => setMember([...member, value])) ;
+    }
+
+  }, [groupDetails])
+
+  console.log('-------member state----------', member);
     return(
+      <>
+      <Card>
+        <Card.Header>
+          {groupDetails.name}
+        </Card.Header>
+        <Card.Body>
+          {groupDetails.description}
+        </Card.Body>
+      </Card>
 
       <Table striped bordered hover>
         <thead>
+        <tr>
+         <td colSpan='4'>
+         Members
+         </td>
+        </tr>
           <tr>
-
-
-
+          
             <th> Name</th>
-            <th>Description</th>
-            <th> Bank Account</th>
-            <th>Contribution Frequency</th>
-            <th>Total Members</th>
-            <th>Staments</th>
-
-
+            <th>Phone Number</th>
+            <th>Total Contribution</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
-        <Each obj={groupDetails} key={groupDetails.id}/>
+        {member.length !== 0 ? member[0].map(memberDetail =>   <Each obj={memberDetail} key={memberDetail.userId}/>) : <Spinner className="mx-auto text-center" animation="border" role="status">
+  <span className="sr-only">Loading...</span>
+</Spinner>}
+
         </tbody>
       </Table>
-
+      </>
     )
 
 
