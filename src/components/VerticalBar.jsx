812a6b9ed1aaf67f624e448b824f1cace54c2558
Anillab <anillabusolo@gmail.com>
2020-03-30 23:20:59 +0000

    fetched data from api

--- src/components/VerticalBar.jsx
+++ src/components/VerticalBar.jsx
@@ -1,68 +1,49 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
-
+import axios from 'axios';
 class VerticalPage extends React.Component {
-  state = {
-    dataBar: {
-      labels: ["Semi-Formal", "Informal", "Formal"],
-      datasets: [
-        {
-          label: "Contributions",
-          data: [12, 19, 3],
-          backgroundColor: [
-            "rgba(255, 134,159,0.4)",
-            "rgba(98,  182, 239,0.4)",
-            "rgba(255, 218, 128,0.4)"
-
-          ],
-          borderWidth: 2,
-          borderColor: [
-            "rgba(255, 134, 159, 1)",
-            "rgba(98,  182, 239, 1)",
-            "rgba(255, 218, 128, 1)"
-
-          ]
-        }
-      ]
-    },
-    barChartOptions: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        xAxes: [
-          {
-            barPercentage: 1,
-            gridLines: {
-              display: true,
-              color: "rgba(0, 0, 0, 0.1)"
-            }
-          }
-        ],
-        yAxes: [
-          {
-            gridLines: {
-              display: true,
-              color: "rgba(0, 0, 0, 0.1)"
-            },
-            ticks: {
-              beginAtZero: true
-            }
-          }
-        ]
-      }
-    }
+  constructor(props){
+    super(props);
+    this.state={Groups:{}};
   }
+  componentDidMount(){
+    axios.get(`https://hidden-atoll-66913.herokuapp.com/api/groups`)
+             .then(res=>{
+               const total = res.data;
+               console.log('---total value----', total);
+               let groupName=[];
+               let amountCon=[];
+               total.groups.forEach(group=>{
+                 groupName.push(group.name);
+                 amountCon.push(group.amount);
+               });
+               console.log(amountCon)
+               console.log(groupName)
+               this.setState({
+                 Groups:{
+                   labels:groupName,
+                   datasets:[
+                     {
+                       label:'Groups Total Contributions',
+                       data:amountCon
+                     }
+                   ]
+                 }
 
+               })
+             }).catch(err => console.log(err));
+  }
 
   render() {
     return (
       <MDBContainer>
         <h3 className="mt-5">Bar chart</h3>
-        <Bar data={this.state.dataBar} options={this.state.barChartOptions} />
+        <Bar data={this.state.Groups}/>
       </MDBContainer>
     );
   }
 }
+
 
 export default VerticalPage;
