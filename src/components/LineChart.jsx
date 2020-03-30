1b8c4759ea1ce8d23b1029e2d754ea5b645cce87
Anillab <anillabusolo@gmail.com>
2020-03-30 16:20:53 +0000

    fetched graph data from the database

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,65 +1,50 @@
 import React from "react";
+import axios from 'axios';
 import { Line } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
 
 class LinePage extends React.Component {
-  state = {
-    dataLine: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "Informal Groups",
-          fill: true,
-          lineTension: 0.3,
-          backgroundColor: "rgba(225, 204,230, .3)",
-          borderColor: "rgb(205, 130, 158)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgb(205, 130,1 58)",
-          pointBackgroundColor: "rgb(255, 255, 255)",
-          pointBorderWidth: 10,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgb(0, 0, 0)",
-          pointHoverBorderColor: "rgba(220, 220, 220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "Formal Groups",
-          fill: true,
-          lineTension: 0.3,
-          backgroundColor: "rgba(184, 185, 210, .3)",
-          borderColor: "rgb(35, 26, 136)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgb(35, 26, 136)",
-          pointBackgroundColor: "rgb(255, 255, 255)",
-          pointBorderWidth: 10,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgb(0, 0, 0)",
-          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    }
-  };
+  constructor(props){
+    super(props);
+    this.state={Groups:{}};
+  }
+  componentDidMount(){
+    axios.get(`https://hidden-atoll-66913.herokuapp.com/api/groups`)
+             .then(res=>{
+               const total = res.data;
+               console.log('---total value----', total);
+               let groupName=[];
+               let totalCon=[];
+               total.groups.forEach(group=>{
+                 groupName.push(group.name);
+                 totalCon.push(group.contributions.length);
+               });
+               console.log(totalCon)
+               console.log(groupName)
+               this.setState({
+                 Groups:{
+                   labels:groupName,
+                   datasets:[
+                     {
+                       label:'Groups Total Contributions',
+                       data:totalCon
+                     }
+                   ]
+                 }
 
-  render() {
-    return (
-      <MDBContainer>
-        <Line data={this.state.dataLine} options={{ responsive: true }} />
-      </MDBContainer>
-    );
+               })
+             }).catch(err => console.log(err));
+  }
+  render(){
+    return(
+      <div>
+      <Line
+      data={this.state.Groups}
+      />
+      </div>
+    )
   }
 }
+
 
 export default LinePage;
