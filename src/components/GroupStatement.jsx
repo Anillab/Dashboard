2cdb8d88169da7901a95805122e940be2107c73a
Anillab <anillabusolo@gmail.com>
2020-03-30 23:20:17 +0000

    added api url

--- src/components/GroupStatement.jsx
+++ src/components/GroupStatement.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,58 +9,81 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Behavior from './Behaivour.jsx';
+const API_URL='https://hidden-atoll-66913.herokuapp.com/api';
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
 
-function createData(name, calories, fat) {
-  return { name, calories, fat};
+class SimpleTable extends React.Component{
+constructor(props){
+  super(props);
+  this.state={
+    group:{},
+    contribution:[]
+  }
+
+}
+componentDidMount(){
+  if(this.props !== null){
+      console.log(this.props);
+  }
+
+
+  let groupId = this.props.match.params.id;
+  axios.get(`${API_URL}/groups/${groupId}`)
+      .then(res=>{
+        this.setState({group: res.data}, () => {
+          this.state.group.contributions.map(value => {
+            return axios.get(`${API_URL}/contributions/${value._id}`)
+          })
+        })
+      })
+      .then(resData => {
+        this.setState({contribution: [...this.state.contribution, resData.data]})
+      })
+      .catch(function (error){
+        console.log(error);
+      })
 }
 
-const rows = [
-  createData('03/03/2020', 5, 1000 ),
-  createData('10/03/2020', 4, 900),
-  createData('17/03/2020', 5, 1100),
-  createData('Total Contribution', '', 3000),
-  createData('Bank Charges', '', 15),
-  createData('Total Amount Available','', 2985),
-
-];
-
-export default function SimpleTable() {
-  const classes = useStyles();
-
-  return (
-    <div>
-    <div>
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date of Contribution</TableCell>
-            <TableCell align="right">Members Contributed</TableCell>
-            <TableCell align="right">Amount Contributed</TableCell>
+render(){
+  console.log(this.props);
+return (
+  <div>
+  <div>
+  <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Date of Contribution</TableCell>
+          <TableCell align="right">Title of Contribution</TableCell>
+          <TableCell align="right">Members Contributed</TableCell>
+          <TableCell align="right">Amount Contributed</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {this.state.contribution.map(contribution => (
+          <TableRow key={contribution.title}>
+            <TableCell component="th" scope="row">
+            a
+            </TableCell>
+            <TableCell align="right">a</TableCell>
+            <TableCell align="right">a</TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
-    <div>
-    <Behavior />
-    </div>
-    </div>
-  );
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+  </div>
+  <div>
+  <Behavior />
+  </div>
+  </div>
+)};
+
 }
+
+export default SimpleTable;
